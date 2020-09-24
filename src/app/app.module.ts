eb25f577e758d6781b5538acfe7c85ced8f63bd9
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-24 07:56:28 -0500

    all object services and auth service, environment setup, and models package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ModelsComponent } from './models/models.component';
+import { OrderComponent } from './services/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ModelsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
