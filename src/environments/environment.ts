afb2c90b81ace1041a2559aa65e54e678fd6760d
dangelo513 <42424566+dangelo513@users.noreply.github.com>
2020-09-24 20:46:22 -0700

    documentation and error handling instructions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,4 +4,22 @@ export const environment = {
   API_URL: 'http://localhost:8080/daniel_richardson_p1/app'
   //set to your own tomcat or aws beanstalk
   //should coincide with servlets
+
+  //if you get a cross origin html error, add this to your web.xml (if we even will need it):
+//   <filter>
+//   <filter-name>CorsFilter</filter-name>
+//   <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>
+//   <init-param>
+//       <param-name>cors.allowed.origins</param-name>
+//       <param-value>*</param-value>
+//   </init-param>
+//   <init-param>
+//       <param-name>cors.allowed.methods</param-name>
+//       <param-value>GET,POST,HEAD,OPTIONS,PUT</param-value>
+//   </init-param>
+// </filter>
+// <filter-mapping>
+//   <filter-name>CorsFilter</filter-name>
+//   <url-pattern>/*</url-pattern>
+// </filter-mapping>
 };
