4134371e6b6f09cf7d5626d248d4202a84dddec6
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-28 17:26:25 -0500

    addToCart method outline

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -5,6 +5,8 @@ import { Principal } from '../models/principal';
 import { map } from 'rxjs/operators';
 import { environment as env } from '../../environments/environment';
 import { Order } from '../models/order';
+import { User } from '../models/user';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ import { Order } from '../models/order';
 export class OrderService {
   // orderArray; //init array
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
     console.log('Instantiating OrderService');
   }
 
@@ -56,6 +58,39 @@ export class OrderService {
     let url = `${env.API_URL}/orders`;
     console.log("URL: "+ url);
     return this.http.put(url,order,{responseType:'json',observe:"response"});
+  }
+
+  addToCart() { // move this wherever. Untested
+    console.log('Adding to cart...');
+    // below should access the user that is logged in
+    let authUser = this.authService.currentUserValue;
+    console.log(authUser);
+    console.log(authUser.username);
+    // How to access the item that was clicked on?
+    
+    // if no order with current user as field exists...
+      // Use OrderService below. May have to create method that is the 
+      // response from /orders/user-id/{id}
+      
+    if (true) { // replace the word true with logic.
+
+      // CREATE new order.
+      // user field should be "authUser".
+
+      // CREATE orderedItem.
+      // order field matches the order just created above.
+      // item field matches item that the button is on.
+
+    } else { // if this block executes, user already has an order
+
+      // locate the order that exists (what if the user has multiple orders?)
+      // UPDATE that order with the itemCount++ and the price += item's price
+      // orderService.Update(updatedOrder)
+
+      // CREATE an orderedItem with the same item_id as the item button is on and
+      // same order_id as the order that exists (again, what if multiple orders?)
+
+    }
   }
 
 
