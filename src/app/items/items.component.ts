d1a2d17bc3038ad374cf88257d18ce32e762a9eb
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-28 15:40:58 -0500

    genre_id to genre in item component

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -72,13 +72,13 @@ export class ItemsComponent implements OnInit {
   }  
   // ag-grid ==================
 columnDefs = [
-  { field: 'item_id', sortable: true, filter: true, resizable: true  },
+  { field: 'id', sortable: true, filter: true, resizable: true  },
   { field: 'name', sortable: true, filter: true, resizable: true  },
   { field: 'price', sortable: true, filter: true, resizable: true  },
   { field: 'stock', sortable: true, filter: true, resizable: true  },
   { field: 'description', sortable: true, filter: true, resizable: true  },
   { field: 'itemImageUrl', sortable: true, filter: true, resizable: true  },
-  { field: 'genre_id', sortable: true, filter: true, resizable: true }
+  { field: 'genre', sortable: true, filter: true, resizable: true }
 ];
 
 rowData: any;
@@ -87,13 +87,13 @@ rowData: any;
   //functions ==================
   updateItem(){
     let updatedItem = new Item;
-    updatedItem.$item_id = this.updateFields.idUpdate.value;
+    updatedItem.$id = this.updateFields.idUpdate.value;
     updatedItem.$name = this.updateFields.nameUpdate.value;
     updatedItem.$price = this.updateFields.priceUpdate.value;
     updatedItem.$stock = this.updateFields.stockUpdate.value;
     updatedItem.$description = this.updateFields.descriptionUpdate.value;
     updatedItem.$itemImageUrl = this.updateFields.urlUpdate.value;
-    updatedItem.$genre_id = this.updateFields.genreUpdate.value;
+    updatedItem.$genre = this.updateFields.genreUpdate.value;
     console.log("Updated item: "+ updatedItem);
     this.itemService.updateTargetItem(updatedItem)
     .subscribe(
@@ -114,8 +114,8 @@ rowData: any;
     createdItem.$stock = this.createFields.stockCreate.value;
     createdItem.$description = this.createFields.descriptionCreate.value;
     createdItem.$itemImageUrl = this.createFields.urlCreate.value;
-    createdItem.$genre_id = this.createFields.genreCreate.value;
-    console.log("Create item: "+ createdItem);
+    createdItem.$genre = this.createFields.genreCreate.value;
+    console.log("Create item: "+ createdItem.genre);
     this.itemService.createItem(createdItem)
     .subscribe(
       () => {
@@ -153,13 +153,13 @@ rowData: any;
 
             for( let i = 0; i < length ; i++){
               let newItem = new Item();
-              newItem.$item_id = respJSON[i].id; //FIX THIS depending on what backend sends back
+              newItem.$id = respJSON[i].id; //FIX THIS depending on what backend sends back
               newItem.$name = respJSON[i].name;
               newItem.$price = respJSON[i].price;
               newItem.$stock = respJSON[i].stock;
               newItem.$description = respJSON[i].description;
               newItem.$itemImageUrl = respJSON[i].itemImageUrl;
-              newItem.$genre_id = respJSON[i].genre;
+              newItem.$genre = respJSON[i].genre;
               this.items.unshift(newItem); //adding item to array
               console.log('Added item with id of: '+ respJSON[i]["id"]);
             }
@@ -187,13 +187,13 @@ rowData: any;
           console.log('Response:'+resp.status);
           if(resp.status == 200){ 
             let targetItem = new Item();
-            targetItem.$item_id = this.tgtItem[0].id; //FIX THIS depending on what backend sends back
+            targetItem.$id = this.tgtItem[0].id; //FIX THIS depending on what backend sends back
             targetItem.$name = this.tgtItem[0].firstname;
             targetItem.$price = this.tgtItem[0].lastname;
             targetItem.$stock = this.tgtItem[0].email;
             targetItem.$description = this.tgtItem[0].Itemname;
             targetItem.$itemImageUrl = this.tgtItem[0].password;
-            targetItem.$genre_id = this.tgtItem[0].genre_id;
+            targetItem.$genre = this.tgtItem[0].genre;
             console.log('Added item with id of: '+ this.tgtItem[0]["id"]);
           }
       },
