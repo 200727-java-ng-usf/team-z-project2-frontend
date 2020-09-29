67bc9a9a96a403b27d330a3e197d4a120922a542
dangelo513 <42424566+dangelo513@users.noreply.github.com>
2020-09-28 20:58:30 -0700

    basic card framework in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+//MODULES
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular'; //ag-grid API: if not installed, use npm install --save ag-grid-community ag-grid-angular
@@ -13,8 +14,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       //there could be file path issues depending on where the node_modules folder was placed
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+//angular cards
+// import { CardMaterialModule } from './material.module';
+// import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 // import { ModelsComponent } from './models/models.component';
@@ -30,6 +36,8 @@ import { ItemsComponent } from './items/items.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderedItemsComponent } from './ordered-items/ordered-items.component';
 import { HotProductComponent } from './hot-product/hot-product.component';
+import { ShopGalleryComponent } from './shop-gallery/shop-gallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -47,16 +55,19 @@ import { HotProductComponent } from './hot-product/hot-product.component';
     ItemsComponent,
     OrdersComponent,
     OrderedItemsComponent,
-    HotProductComponent
+    HotProductComponent,
+    ShopGalleryComponent
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
     HttpClientModule,
-    AgGridModule.withComponents([])
+    AgGridModule.withComponents([]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shop-gallery/shop-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-gallery',
+  templateUrl: './shop-gallery.component.html',
+  styleUrls: ['./shop-gallery.component.css']
+})
+export class ShopGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
