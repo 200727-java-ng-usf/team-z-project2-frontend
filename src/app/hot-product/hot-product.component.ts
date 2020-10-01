1a0993c01dfa4d860b9233da1c5f4368fd9b1df9
dangelo513 <42424566+dangelo513@users.noreply.github.com>
2020-09-30 22:10:51 -0700

    merged item gallery into hot products, current remove from cart bug

--- src/app/hot-product/hot-product.component.ts
+++ src/app/hot-product/hot-product.component.ts
@@ -3,6 +3,8 @@ import { ItemService } from '../services/item.service';
 import { Item } from '../models/item';
 import {StorageService} from '../services/storage.service'
 import { OrderService } from '../services/order.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 declare let $:any;
 
@@ -18,8 +20,18 @@ export class HotProductComponent implements OnInit {
   targetItem = new Item;
   rowData: any;
   newItem = new Item();
+  //new:
+  userdash = '/userdashboard';
+  itemCount:number = 0;
+  subtotal:number = 0;
 
-  constructor(private itemService:ItemService,public storage:StorageService,private orderService: OrderService) { }
+  constructor(
+    private itemService:ItemService,
+    public storage:StorageService,
+    private orderService: OrderService,
+    private router: Router,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
 
@@ -79,18 +91,28 @@ export class HotProductComponent implements OnInit {
     // // return itemArray;
   }
 
-  doAdd(item:Item){
+doAdd(item:Item){
+  this.isAddItem = true;
+  this.hotProduct.push(item)
+  this.storage.set('hotproduct',this.hotProduct);
+  //new:
+  this.subtotal += parseFloat(item.price);        //update subtotal
+  this.itemCount++;                               //increase item count
+  this.storage.set('itemCount', this.itemCount);  //update count to storage
+  this.storage.set('subtotal', this.subtotal);    //and the subtotal
 
-     this.isAddItem = true;
-     
-      this.hotProduct.push(item)
-
-      this.storage.set('hotproduct',this.hotProduct);
-
-    
+  console.log(this.subtotal);
 }
 
 deleteData(key){
+
+  //can't figure this part out 
+  //new:
+  // let deletedItem = this.hotProduct[key];
+  // this.subtotal -= parseFloat(key.price); //update subtotal
+  // this.itemCount--;                               //decrease item count
+  // this.storage.set('itemCount', this.itemCount);  //update count to storage
+  // this.storage.set('subtotal', this.subtotal);    //and the subtotal
 
   this.hotProduct.splice(key,1);   // delete 1 item from position key
 
@@ -99,16 +121,26 @@ deleteData(key){
     this.isAddItem = false;
   }
 
-}
-
-
-
-submitOrder(item){
-
-  this.orderService.addToCart(item);
-
 
 }
+
+
+// instead, we can just send them straight back to their dashboard and checkout there
+// added extra logic to add and delete to handle pricing and itemcount
+
+submitOrder(){
+  // this.orderService.addToCart(item);
+
+  // asks auth service if theyre authenticated, and if not, alert them
+    //if they are logged in, they redirect to their dashboard (checkout)
+  if(this.authService.currentUserValue!=null){
+    this.router.navigate(['/userdashboard']);
+  } else {
+    alert("You must be logged in!");
+  }
+  
+}
+
 
 buildItemArray(){
   this.itemService.getAllItems().subscribe(
